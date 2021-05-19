b9e7550b22c19872c1c86cbe3e9ee1c2d00a47ed
nakamura196 <na.kamura.1263@gmail.com>
2021-05-19 13:35:39 +0900

    rm unused library

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-import sys
-import bs4
-import hashlib
 import json
-import bs4
 import requests
-import time
-import os
-import urllib.parse
-import csv
-import glob
-from rdflib import URIRef, BNode, Literal, Graph
-from rdflib.namespace import RDF, RDFS, FOAF, XSD
-from rdflib import Namespace
-import pandas as pd
-import uuid
-import bs4
-import hashlib
 import argparse
 
 def getValue(item, field):
