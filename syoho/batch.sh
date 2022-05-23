b64c73f15baf39c0b022365fb2a80f7ecfdc7280
nakamura196 <na.kamura.1263@gmail.com>
2022-05-24 07:56:22 +0900

    feat: add data

--- syoho/batch.sh
+++ syoho/batch.sh
@@ -1,8 +1,20 @@
 year=2021
 gas=https://script.google.com/macros/s/AKfycbxnoyx_t15Ve7ItC8L3KspKBqOJAbYcAuqfmR6KjG7zasdGxyQ-65vhR39C6NX4xdxyxQ/exec
-# python main.py ippan $year $gas
-# python main.py tokutei $year $gas
+
+echo "python main.py ippan $year $gas"
+python main.py ippan $year $gas
+
+echo "python main.py tokutei $year $gas"
+python main.py tokutei $year $gas
+
+echo "python read.py ippan $year $gas"
 python read.py ippan $year $gas
+
+echo "python read.py tokutei $year $gas"
 python read.py tokutei $year $gas
+
+echo "python combine.py ippan $year"
 python combine.py ippan $year
+
+echo "python combine.py tokutei $year"
 python combine.py tokutei $year
